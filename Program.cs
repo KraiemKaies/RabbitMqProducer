5d3ed792c3fe5b1354f95295818ee14c02d70ccf
kraiemkaies <kraiem.kaies@gmail.com>
2022-03-02 12:55:24 +0100

    Create producer

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace RabbitMQ.Producer
+﻿using System.Text;
+using Newtonsoft.Json;
+using RabbitMQ.Client;
+namespace RabbitMQ.Producer
 {
     static class Program
     {
         static void Main(string[] args)
         {
-
+            var factory = new ConnectionFactory
+            {
+                Uri = new Uri("amqp://kraiem:kraiem@localhost:5672")
+            };
+            using var connection = factory.CreateConnection();
+            using var channel = connection.CreateModel();
+            channel.QueueDeclare("Demo-queue",
+                durable: true,
+                exclusive: false,
+                autoDelete: false,
+                arguments:null);
+            var message = new { Name = "Producer", Message = "Hello !"};
+            var body = Encoding.UTF8.GetBytes(JsonConvert.SerializeObject(message));
+            channel.BasicPublish("","demo-queue",null,body);
         }
     }
 }
